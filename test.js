5b33ca2537baa4995cb20f20e0f5d76f2fb83f5e
extreme <bkhattri16@gmail.com>
2025-01-19 08:15:19 +0545

    new file:   sum.js     new file:   test.js 	new file:   test.py

---
+++ test.js
@@ -0,0 +1,44 @@
+// class myclass{
+//     constructor(name){
+//         this.name=name
+//     }
+//     getname(){
+//         console.log(this.name)
+//     }
+// }
+// const myobj=new myclass("ram")
+// myobj.getname()
+
+function evenodd(n){
+    if(n%2==0){
+        console.log("even")
+    }
+    else{
+        console.log("odd")
+    }
+}
+evenodd(5)
+
+let evenOdd=function(n){
+    if(n%2==0){
+        console.log("even")
+    }
+    else{
+        console.log("odd")
+    }
+}
+evenodd(6)
+
+
+let sum=(n)=>  {
+
+  if(n%2==0){
+        console.log("even")
+    }
+    else{
+        console.log("odd")
+    }
+}
+sum(7)
+
+
