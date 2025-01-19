aa50b8faa1cf96054c617db8da8937314ff8b8d5
extreme <bkhattri16@gmail.com>
2025-01-19 08:37:36 +0545

    mycommit on second branch

---
+++ abc.js
@@ -0,0 +1,16 @@
+let sum=(a,b)=>a+b
+console.log(sum(2,3))
+
+const diff=()=>{
+    console.log("constant function")
+}
+diff()
+
+let summ=(a,b)=>{
+    console.log(a+b)
+}
+summ(5,6)
+let v=[5,4,2,3]
+v.sort()
+console.log(v)
+console.log(diff)
